df557bcb41921e7bee6c450836ce1c986adf0dd3
michaeltumbaco <111886904+michaeltumbaco@users.noreply.github.com>
2022-12-15 22:55:26 -0500

    Add files via upload

---
+++ Codeplug of Basic Commands and Arrays.php
@@ -0,0 +1,14 @@
+<?php
+$MyFirstVariable = "Michael";
+
+echo 'My first variable was named $MyFirstVariable ';
+
+echo "The value of my variable was $MyFirstVariable ";
+
+$empty= array();
+
+$array= array("Michael", "Tumbaco", "Fall2022");
+
+$numbers = array_fill(1,50);
+
+?>
